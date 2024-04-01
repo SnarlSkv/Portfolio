888427516013ad8585869b8ea4fa6a46e5ef4576
Serhii Kuzub <kuzub2431@gmail.com>
2024-04-01 15:17:41 +0300

    upd

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,39 +1,32 @@
 import Work1 from '../../assets/work1.jpg';
 import Work2 from '../../assets/work2.jpg';
-import Work3 from '../../assets/work3.jpg';
-import Work4 from '../../assets/work4.jpg';
-import Work5 from '../../assets/work5.jpg';
+import Work3 from '../../assets/work4.jpg';
+import Work4 from '../../assets/work5.jpg';
 
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Web design",
+    title: "Portfolio",
     category: "web",
   },
   {
     id: 2,
     image: Work2,
-    title: "App movil",
+    title: "Burger Shop",
     category: "app",
   },
   {
     id: 3,
     image: Work3,
-    title: "Brand design",
-    category: "design",
-  },
-  {
-    id: 4,
-    image: Work4,
     title: "App movil",
     category: "app",
   },
   {
-    id: 5,
-    image: Work5,
-    title: "Web design",
+    id: 4,
+    image: Work4,
+    title: "Modern WebSite",
     category: "web",
   },
 ];
@@ -48,8 +41,5 @@ export const projectsNav = [
 	},
 	{
 		name: 'app',
-	},
-	{
-		name: 'design',
-	},
+	}
 ]
